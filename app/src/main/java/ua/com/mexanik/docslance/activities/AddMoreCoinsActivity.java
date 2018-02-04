d5021e4a738789f4af2393abf57aa12b9b62c7ae
kostyaBoss <kostyasharp787898@gmail.com>
2018-02-04 02:13:29 +0200

    - Added magic button

--- app/src/main/java/ua/com/mexanik/docslance/activities/AddMoreCoinsActivity.java
+++ app/src/main/java/ua/com/mexanik/docslance/activities/AddMoreCoinsActivity.java
@@ -1,8 +1,10 @@
 package ua.com.mexanik.docslance.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
+import in.shadowfax.proswipebutton.ProSwipeButton;
 import ua.com.mexanik.docslance.R;
 
 public class AddMoreCoinsActivity extends AppCompatActivity {
@@ -11,5 +13,20 @@ public class AddMoreCoinsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_more_coins);
+
+        final ProSwipeButton proSwipeBtn = (ProSwipeButton) findViewById(R.id.awesome_btn);
+        proSwipeBtn.setOnSwipeListener(new ProSwipeButton.OnSwipeListener() {
+            @Override
+            public void onSwipeConfirm() {
+                // user has swiped the btn. Perform your async operation now
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // task success! show TICK icon in ProSwipeButton
+                        proSwipeBtn.showResultIcon(true); // false if task failed
+                    }
+                }, 2000);
+            }
+        });
     }
 }
