4da64b104e6d146b62236bc6228e7f08e66ef18b
SverbliukRoman <amor201075@hotmail.com>
2018-02-04 12:22:58 +0200

    com

--- app/src/main/java/ua/com/mexanik/docslance/fragments/fragments/FragmentProfile.java
+++ app/src/main/java/ua/com/mexanik/docslance/fragments/fragments/FragmentProfile.java
@@ -2,8 +2,11 @@ package ua.com.mexanik.docslance.fragments.fragments;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +19,9 @@ import com.bumptech.glide.Glide;
 import ua.com.mexanik.docslance.Constants;
 import ua.com.mexanik.docslance.R;
 
+import static ua.com.mexanik.docslance.Constants.PREFS_PROFILE_FIRST_NAME;
+import static ua.com.mexanik.docslance.Constants.PREFS_PROFILE_IMAGE_URL;
+
 
 public class FragmentProfile extends Fragment {
 
@@ -27,6 +33,8 @@ public class FragmentProfile extends Fragment {
     public TextView gas;
     public ImageView avatarImageView;
     public RatingBar ratingBar;
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
     //public ImageButton imageButton;
     Intent intent;
 
@@ -47,15 +55,24 @@ public class FragmentProfile extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         avatarImageView = view.findViewById(R.id.person_image);
-        name = view.findViewById(R.id.name);
+        name = view.findViewById(R.id.namePro);
         specialization = view.findViewById(R.id.specialization);
         age = view.findViewById(R.id.age);
         experience = view.findViewById(R.id.experience);
         education = view.findViewById(R.id.education);
         gas = view.findViewById(R.id.gas);
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        editor = prefs.edit();
 
-        Glide.with(this).load(Constants.PROBA_DOCTOR_FOTO).
+        // get prefs for loading
+        String profileImageUrl = prefs.getString(PREFS_PROFILE_IMAGE_URL, "");
+        Log.d("URL", profileImageUrl);
+        String profileName = prefs.getString(PREFS_PROFILE_FIRST_NAME, "");
+
+        name.setText(getString(R.string.greetings) + ", " + "Костя Боскін" + " " + "!");
+
+        Glide.with(this).load(profileImageUrl).
                 into(avatarImageView);
         gas.setText("Баланс мого кошелька: " + Constants.PROBA_DOCTOR_GAS);
         name.setText(Constants.PROBA_DOCTOR_NAME);
